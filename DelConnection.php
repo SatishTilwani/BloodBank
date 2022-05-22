2ced267dc414506f19eed53ea3e94793c1f3d584
SatishTilwani <106021091+SatishTilwani@users.noreply.github.com>
2022-05-22 09:37:14 -0700

    Add files via upload

---
+++ DelConnection.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "config.php";
+session_start();
+$a1=$_GET['t1']; //cid
+$a2=$_GET['t2']; //rid
+
+$sql="DELETE FROM `ctd` WHERE cid_fk=$a1 and rid_fk=$a2 and ctdstatus=1";
+if($stmt = mysqli_prepare($connect, $sql)){
+
+    if(mysqli_stmt_execute($stmt)==true){
+        
+      $sql1="UPDATE req SET reqstatus=0 WHERE rid=$a2";
+        if($stmt1 = mysqli_prepare($connect, $sql1)){
+
+            if(mysqli_stmt_execute($stmt1)==true){
+        
+            header("location: connectclients.php");
+        } 
+      }
+    }  
+  }
+
+
+?>
